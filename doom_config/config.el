7de678e4bd3c426729a7c505db82efc291bb52ca
Jiejing Zhang <jiejing.zjj@alibaba-inc.com>
2021-09-04 22:05:05 +0800

    add markdown live preview.

--- doom_config/config.el
+++ doom_config/config.el
@@ -31,6 +31,11 @@
 ;; 解决中文卡顿的问题.
 (set-language-environment 'utf-8)
 
+(defun markdown-html (buffer)
+  (princ (with-current-buffer buffer
+    (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+  (current-buffer)))
+
 ;;; 防止tramp出现小文件打不开的
 (setq tramp-inline-compress-start-size 1000000)
 
